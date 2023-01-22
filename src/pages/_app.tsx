1ab07e1d286b202136e643e4542a8d1d8ca1650e
Joel Soliz <joelsolizchoque@gmail.com>
2023-01-22 10:59:23 -0400

    Modify App to have Theme context.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { ColorModeContext, useMode } from "@/styles/theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Sidebar from "@/components/global/sidebar";
+import TopBar from "@/components/global/topbar";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    const [theme, colorMode] = useMode();
+
+    return (
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Sidebar />
+                <div className="content">
+                    <TopBar />
+                    <Component {...pageProps} />
+                </div>
+            </ThemeProvider>
+        </ColorModeContext.Provider>
+    );
 }
